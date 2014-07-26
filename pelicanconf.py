d6f0ea53050d3e28d1677898e76bdecf96c16108
arunk-s <arunsori94@gmail.com>
2014-07-26 15:14:47 +0530

    Added DISCUS_SITENAME

--- pelicanconf.py
+++ pelicanconf.py
@@ -14,6 +14,7 @@ TIMEZONE = 'Asia/Kolkata'
 DEFAULT_LANG = u'en'
 THEME = '/home/arun/Work/blog/pelican-themes/pure-single'
 COVER_IMG_URL = 'http://i.imgur.com/jGxiPTB.jpg'
+DISQUS_SITENAME = "mycrossroadsandencounters"
 
 #COVER_IMG_URL = 'http://i.imgur.com/nPLEHnN.jpg'
 # Feed generation is usually not desired when developing
@@ -22,8 +23,8 @@ CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
 # Blogroll
-LINKS = (('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),)
+#LINKS = (('Python.org', 'http://python.org/'),
+#         ('Jinja2', 'http://jinja.pocoo.org/'),)
 
 # Social widget
 SOCIAL = (
